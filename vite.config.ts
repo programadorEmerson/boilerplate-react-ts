ed2f827276930c4a51278d978651e5c15ac0bb05
programadorEmerson <programador.emerson@hotmail.com>
2024-04-14 10:18:53 -0300

    chore(vite.config): vite.config initial configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import fs from 'fs';
+import path from 'path';
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
+const certFile = './local.pem';
+const keyFile = './local-key.pem';
+
 export default defineConfig({
+  base: '/',
   plugins: [react()],
-})
+  server: {
+    host: 'local.meusite.com.br',
+    port: 3000,
+    https: {
+      key: fs.readFileSync(keyFile),
+      cert: fs.readFileSync(certFile),
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  }
+});
